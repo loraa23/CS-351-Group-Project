64739f2e28cea5c6334c720c0c8693eb191dba5c
Rishi Shah <rshah270@uic.edu>
2025-10-20 18:06:35 -0500

    Implemented rbt structure, etc

--- backend/schedule/models.py
+++ backend/schedule/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # holds each class or commute time block
 class Event(models.Model):
     title = models.CharField(max_length=200)
-    # location = models.CharField(max_length=200, blank=True)
-    # start_time = models.DateTimeField()
-    # end_time = models.DateTimeField()
-    # building = models.CharField(max_length=50, blank=True)
-    # room = models.CharField(max_length=20, blank=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    building = models.CharField(max_length=50, blank=True, null=True)
+    room = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -16,4 +16,3 @@ class Event(models.Model):
 class UICSchedule(models.Model):
     file = models.FileField(upload_to='uic_schedules/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    
--- backend/schedule/rbtree.py
+++ backend/schedule/rbtree.py
@@ -1,1 +1,62 @@
-# define and implement red black tree here (each node value is an Event)
+# Define and implement red-black tree here (each node value is an Event)
+
+RED = True
+BLACK = False
+
+class RBNode:
+    def __init__(self, event):
+        self.event = event
+        self.left = None
+        self.right = None
+        self.parent = None
+        self.color = RED
+
+class RedBlackTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, event):
+        new_node = RBNode(event)
+
+        if self.root is None:
+            new_node.color = BLACK 
+            self.root = new_node
+            return
+
+        current = self.root
+        while True:
+            if event.start_time < current.event.start_time:
+                if current.left is None:
+                    current.left = new_node
+                    new_node.parent = current
+                    break
+                current = current.left
+            else:
+                if current.right is None:
+                    current.right = new_node
+                    new_node.parent = current
+                    break
+                current = current.right
+
+        # self._fix_insert(new_node)  # balancing to be implemented later
+
+    def inorder(self):
+        result = []
+        self._inorder_helper(self.root, result)
+        return result
+
+    def _inorder_helper(self, node, result):
+        if node:
+            self._inorder_helper(node.left, result)
+            result.append(node.event)
+            self._inorder_helper(node.right, result)
+
+    # Rotation and balancing
+    def _left_rotate(self, node):
+        pass
+
+    def _right_rotate(self, node):
+        pass
+
+    def _fix_insert(self, node):
+        pass
